a0afe68637f7826ec06dde772c16656f556a4a4b
hNESJJZBDDHS8 <102894612+hNESJJZBDDHS8@users.noreply.github.com>
2022-04-05 14:15:31 +0500

    Add files via upload

---
+++ Pg.py
@@ -0,0 +1,185 @@
+import os,sys
+try: import requests
+except ModuleNotFoundError:print("Aking Install Module requests");os.system("python -m pip install requests &> /dev/null")
+try: import bs4
+except ModuleNotFoundError:print("Aking Install Module bs4");os.system("python -m pip install bs4 &> /dev/null")
+try: import mechanize
+except ModuleNotFoundError:print("Aking Install Module mechanize");os.system("python -m pip install mechanize &> /dev/null")
+try: import gTTS
+except ModuleNotFoundError: os.system("python -m pip install gTTS &> /dev/null")
+## import in
+import os,sys,time,datetime,re,random,hashlib,threading,json,getpass,urllib,requests
+import os, time, uuid, requests
+from multiprocessing.pool import ThreadPool
+os.system("clear")
+os.system("termux-setup-storage")
+import requests as req
+import requests as re
+import time,random,json
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,ipaddress,calendar,requests,mechanize,bs4,sys,os,subprocess,uuid,requests,sys,random,time,re,base64,json,platform
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+from random import random as acak
+from random import choice as pilih
+from random import randint
+from bs4 import BeautifulSoup
+import requests as ress
+from sys import exit as exit
+class jalan:
+	def __init__(self, z):
+		for e in z + "\n":
+			sys.stdout.write(e)
+			sys.stdout.flush()
+			time.sleep(0.009)
+## import crack
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor as zthreads
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+from concurrent.futures import ThreadPoolExecutor as kikygtg
+from requests.exceptions import ConnectionError
+from bs4 import BeautifulSoup as parser
+from bs4 import BeautifulSoup as par
+from requests.exceptions import ConnectionError
+try:os.remove("old.txt")
+except:pass
+try:os.remove("oldv2.txt")
+except:pass
+try:
+	os.mkdir('dump')
+except:pass
+try:
+	os.mkdir('result')
+except:pass
+try:
+	os.mkdir('/sdcard/')
+except:pass
+try:
+	import requests,calendar
+except ModuleNotFoundError:
+	os.system("python -m pip install requests ")
+try:
+	import bs4
+except ModuleNotFoundError:
+	os.system("python -m pip install bs4 ")
+try:
+	import mechanize
+except ModuleNotFoundError:
+	os.system("python -m pip install mechanize ")
+
+import requests,bs4,sys,os,random,time,re,json,uuid,subprocess,platform,base64
+from random import randint
+from concurrent.futures import ThreadPoolExecutor
+from bs4 import BeautifulSoup
+from datetime import date
+from datetime import datetime
+from urllib.parse import quote
+import requests,bs4,sys,os,random,time,re,json,uuid,subprocess
+from random import randint
+import requests, re, os, time
+def line_chack_dote():
+	m = [".","..","...","....","....."]
+	for b in range(2):
+		for t in m:
+				sys.stdout.write("\r[*] Creating TNL Internet File " + t)
+	sys.stdout.flush()
+	time.sleep(0.1)
+
+def R():
+			os.system("clear")
+			logo()
+			print("        \033[1;92m 𝙱𝚈𝙿𝙰𝚂𝚂𝙴𝙳 𝙱𝚈 𝙰𝙻𝙴𝚇 𝙱𝙾𝚈 + 𝙳𝙸𝙽𝙾 𝙺𝙸𝙽𝙶 😎 \033[1;97m")
+			print("          \033[1;92m 𝚁𝙰𝙼𝚉𝙰𝙽 𝙺𝙴 𝙰𝙺𝙺 𝙲𝙷𝙾𝚃𝙴 𝚂𝙴 𝙶𝙸𝙵𝚃 ❣️ \033[1;97m")
+			print (' [🔥]⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑')
+			print(" [01]\033[1;32m 𝙱𝚈𝙿𝙰𝚂𝚂\033[1;32m    [𝚀𝙰𝙸𝚂𝙴𝚁]  ")
+			print(" [02]\033[1;32m 𝙱𝚈𝙿𝙰𝚂𝚂\033[1;33m    [𝚀𝙰𝙳𝙸𝚁]  ")
+			print(" [03]\033[1;32m 𝙱𝚈𝙿𝙰𝚂𝚂\033[1;34m    [𝚂𝚂𝙱]  ")
+			print(" [04]\033[1;32m 𝙱𝚈𝙿𝙰𝚂𝚂\033[1;31m    [𝙰𝙳𝙵]  ")
+			print(" [05]\033[1;32m 𝙱𝚈𝙿𝙰𝚂𝚂\033[1;35m    [𝙱𝚂𝙽]  ")
+			print(" [06]\033[1;32m 𝙱𝚈𝙿𝙰𝚂𝚂\033[1;36m    [𝙰𝙺𝙸𝙽𝙶]  ")
+			print(" [07]\033[1;32m 𝙱𝚈𝙿𝙰𝚂𝚂\033[1;37m    [𝙰𝙱𝙳𝚄𝙻𝙻𝙰𝙷]  ")
+			print(" [08]\033[1;32m 𝙱𝚈𝙿𝙰𝚂𝚂\033[1;38m    [𝚉𝙰𝙷𝙸𝙳]  ")
+			print(" [09]\033[1;32m 𝙱𝚈𝙿𝙰𝚂𝚂\033[1;34m    [𝙹𝙰𝙼𝙴𝚂]  ")
+			print(" [10]\033[1;32m 𝙱𝚈𝙿𝙰𝚂𝚂\033[1;96m    [𝚂𝚄𝙱𝙷𝙰𝙽]  ")
+			print(" [11]\033[1;32m 𝙱𝚈𝙿𝙰𝚂𝚂\033[1;38m    [𝚉𝚄𝙱𝙸]")
+			print(" [12]\033[1;32m 𝙱𝚈𝙿𝙰𝚂𝚂\033[1;33m    [𝙰𝙳𝙸-𝙿𝙰𝙸𝙳]  ")
+			print(" [13]\033[1;32m 𝙱𝚈𝙿𝙰𝚂𝚂\033[1;34m    [𝙵𝙲 𝙿𝚁𝙾 𝙼𝙰𝚇\033[1;37m]  ")
+			
+			print (' [🔥]⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑')
+			key = input(" [•] 𝙲𝚑𝚘𝚜𝚜𝚎 : ")
+			print (' [🔥]⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑⌑')
+			if key in [""]:
+				print (" [🔥] 𝙿𝙻𝙴𝙰𝚂𝙴 𝚂𝙴𝙻𝙴𝙲𝚃 𝙲𝙾𝚁𝚁𝙴𝙲𝚃 𝙾𝙿𝚃𝙸𝙾𝙽")
+				time.sleep(1)
+				R()
+				
+			elif key in ["1", "01"]:
+				os.system("git clone https://github.com/hNESJJZBDDHS8/Bypass")
+				os.system("cd Bypass && python Fuck_Qaiser.py")
+			elif key in ["2", "02"]:
+				os.system("git clone https://github.com/hNESJJZBDDHS8/Bypass")
+				os.system("cd Bypass && python Fuck_Qadir.py")
+			elif key in ["3", "03"]:
+				os.system("git clone https://github.com/hNESJJZBDDHS8/Bypass")
+				os.system("cd Bypass && python Fuck_SSB.py")
+			elif key in ["4", "04"]:
+				os.system("git clone https://github.com/hNESJJZBDDHS8/Bypass")
+				os.system("cd Bypass && python Fuck_Adf.py")
+			elif key in ["5", "05"]:
+			    os.system("git clone https://github.com/hNESJJZBDDHS8/Bypass")
+			    os.system("cd Bypass && python Fuck_Bsn.py")
+			elif key in ["6", "06" ]:
+				os.system("git clone https://github.com/hNESJJZBDDHS8/Bypass")
+				os.system("cd Bypass && python AKING.py")
+			elif key in ["7", "07" ]:
+				os.system("git clone https://github.com/hNESJJZBDDHS8/Bypass")
+				os.system("cd Bypass && python Fuck_Abdullah.py")
+			elif key in ["8", "08" ]:
+				os.system("git clone https://github.com/hNESJJZBDDHS8/Bypass")
+				os.system("cd Bypass && python Fuck_Zahid.py")
+			elif key in ["9", "09" ]:
+				os.system("git clone https://github.com/hNESJJZBDDHS8/Bypass")
+				os.system("cd Bypass && python Fuck_James.py")
+			elif key in ["10"]:
+				os.system("git clone https://github.com/hNESJJZBDDHS8/Bypass")
+				os.system("cd Bypass && python Fuck_Subhan.py")
+			elif key in ["11"]:
+				os.system("git clone https://github.com/hNESJJZBDDHS8/Bypass")
+				os.system("cd Bypass && python Fuck_Zubair.py")
+			elif key in ["12"]:
+				os.system("git clone https://github.com/hNESJJZBDDHS8/Bypass")
+				os.system("cd Bypass && python Fuck_Adi.py")
+			elif key in ["13"]:
+				os.system("git clone https://github.com/hNESJJZBDDHS8/Bypass")
+				os.system("cd Bypass && python Fuck_Azim.py")
+				
+	
+
+banner="""\033[1;37m
+    ██████  ██    ██ ██████   █████  ███████ ███████ 
+    ██   ██  ██  ██  ██   ██ ██   ██ ██      ██      
+    ██████    ████   ██████  ███████ ███████ ███████ 
+    ██   ██    ██    ██      ██   ██      ██      ██ 
+    ██████     ██    ██      ██   ██ ███████ ███████"""
+ct = datetime.now()
+n = ct.month
+monthsx = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+try:
+	if n < 0 or n > 12:
+		exit()
+	nTemp = n - 1
+except ValueError:
+	exit()
+urls="https://business.facebook.com/business_locations"
+_ses=requests.Session()
+def logo():
+	time.sleep(0.5)
+	os.system("clear")
+	print(banner)
+	print("")
+	time.sleep(0.5)
+    
+if __name__=='__main__':
+	R()
